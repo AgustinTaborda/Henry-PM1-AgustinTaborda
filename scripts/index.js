283bb53018c8dd02597bb31e929919ccd130cbd3
Agustin Taborda <agustinbarto77@gmail.com>
2024-03-20 12:21:36 +1100

    testing

--- scripts/index.js
+++ scripts/index.js
@@ -11,14 +11,12 @@ class Activity {
 // Definiendo la clase Repository
 class Repository {
   constructor () {
-    // this.activities = JSON.parse(localStorage.getItem('activities')) || [];
-    // this.nextId = JSON.parse(localStorage.getItem('nextId')) || 1;
-    this.activities = []
-    this.nextId = 1
+    this.activities = JSON.parse(localStorage.getItem('activities')) || [];
+    this.nextId = JSON.parse(localStorage.getItem('nextId')) || 1;
   }
 
   saveActivities () {
-    // localStorage.setItem('activities', JSON.stringify(this.activities));
+    localStorage.setItem('activities', JSON.stringify(this.activities));
   }
 
   getAllActivities () {
@@ -29,7 +27,7 @@ class Repository {
     const id = this.nextId++
     const activity = new Activity(id, title, description, imgURL)
     this.activities.push(activity)
-    // localStorage.setItem('nextId', JSON.stringify(this.nextId));
+    localStorage.setItem('nextId', JSON.stringify(this.nextId));
   }
 
   deleteActivity (id) {
@@ -46,113 +44,5 @@ class Repository {
     return this.activities
   }
 }
-
-// Inicializando nuestra 'base de datos'
-const repositorio = new Repository()
-
-// Inicializando los selectores que utilizaremos
-const formulario = document.getElementById('formulario')
-const containerTarjetas = document.getElementById('containerTarjetas')
-const buttonAddActivities = document.getElementById('buttonAddActivity')
-
-// Verifiacion de datos del formulario
-const datosForm = function (formulario) {
-  const titulo = formulario.titulo.value
-  const descripcion = formulario.descripcion.value
-  const url = formulario.url.value
-  return { titulo, descripcion, url }
-}
-
-const verificarCampos = function () {
-  const elementosFormulario = formulario.elements
-  for (let i = 0; i < elementosFormulario.length; i++) {
-    if (elementosFormulario[i].nodeName === 'INPUT' && elementosFormulario[i].value === '') {
-      return false
-    }
-  }
-  return true
-}
-
-// Creando el html de las tarjetas de actividades
-const crearTarjeta = function (id, title, description, imgURL) {
-  const nuevaTarjeta = document.createElement('div')
-  const tituloNuevaTarjeta = document.createElement('h3')
-  const descripcionNuevaTarjeta = document.createElement('p')
-  const imagenNuevaTarjeta = document.createElement('img')
-  const basurero = document.createElement('span')
-
-  containerTarjetas.style.setProperty('display', 'flex')
-  nuevaTarjeta.className = 'card'
-  nuevaTarjeta.id = id
-  tituloNuevaTarjeta.textContent = title
-  tituloNuevaTarjeta.className = 'cardTitle'
-  descripcionNuevaTarjeta.textContent = description
-  descripcionNuevaTarjeta.className = 'cardDescription'
-  imagenNuevaTarjeta.src = imgURL
-  basurero.innerHTML = 'X'
-  basurero.className = 'basureroTarjetas'
-  basurero.id = 'basureroTarjetas'
-
-  nuevaTarjeta.appendChild(tituloNuevaTarjeta)
-  nuevaTarjeta.appendChild(imagenNuevaTarjeta)
-  nuevaTarjeta.appendChild(descripcionNuevaTarjeta)
-  nuevaTarjeta.appendChild(basurero)
-  containerTarjetas.appendChild(nuevaTarjeta)
-
-  basurero.addEventListener('click', eliminarTarjetas) // Escuchador del evento eliminar tarjetas
-
-  return nuevaTarjeta
-}
-
-// Creo una funcion que me ayude a renderizar las tarjetas para luego utilizarla cada vez que lo necesite
-const refrescarTarjetas = function () {
-  containerTarjetas.innerHTML = ''
-  const tarjetasHTML = repositorio.getAllActivities().map(activity => {
-    return crearTarjeta(activity.id, activity.title, activity.description, activity.imgURL)
-  })
-  tarjetasHTML.forEach(tarjeta => {
-    containerTarjetas.appendChild(tarjeta)
-  })
-  if (repositorio.activities.length < 1) {
-    containerTarjetas.style.setProperty('display', 'none')
-  }
-  if (repositorio.activities.length > 0) {
-    containerTarjetas.style.setProperty('display', 'flex')
-  }
-}
-
-// Creacion de nuevas tarjetas desde el formulario
-const nuevaActivity = function (event) {
-  event.preventDefault()
-  if (verificarCampos()) {
-    const { titulo, descripcion, url } = datosForm(formulario)
-    repositorio.createActivity(titulo, descripcion, url)
-    formulario.reset()
-    repositorio.saveActivities()
-    refrescarTarjetas()
-  } else {
-    alert('Por favor, completa todos los campos del formulario.')
-  }
-}
-
-buttonAddActivities.addEventListener('click', nuevaActivity)
-
-// Eliminando Tarjetas
-const eliminarTarjetas = function (event) {
-  let elementoClickeado = event.target
-
-  while (elementoClickeado && !elementoClickeado.classList.contains('card')) {
-    elementoClickeado = elementoClickeado.parentElement
-  };
-
-  if (elementoClickeado && elementoClickeado.classList.contains('card')) {
-    const idTarjetaEliminar = elementoClickeado.id
-    repositorio.deleteActivity(idTarjetaEliminar)
-    repositorio.saveActivities()
-    refrescarTarjetas()
-  }
-}.bind(repositorio)
-
-refrescarTarjetas()
 
 module.exports = { Activity, Repository }
--- spec/prueba.spec.js
+++ spec/prueba.spec.js
@@ -1,35 +1,36 @@
-describe('demo', function () {
-  it('Este test debe pasar siempre', function () {
-    expect(4 + 2).toBe(6)
-  })
-})
+// describe('demo', function () {
+//   it('Este test debe pasar siempre', function () {
+//     expect(4 + 2).toBe(6)
+//   })
+// })
 
 const { Repository, Activity} = require('../scripts/index')
 
 describe('La clase Repository', () => {
+
+  let repositorio;
+  beforeEach( function() {
+    repositorio = new Repository()
+  });
+
   it('Debe ser una clase', () => {
     expect(typeof Repository.prototype.constructor).toBe('function')
   })
   it('Debe tener el metodo createActivity', () => {
-    const actividades = new Activity()
-    expect(actividades.createActivity).toBeDefined()
+    expect(repositorio.createActivity).toBeDefined()
   })
   it('El metodo createActivity debe sumar un nuevo elemento ', () => {
-    const actividades = new Activity()
-    actividades.createActivity('a','b','c')
-    expect(actividades.getAllActivities().lenght).toBe(1)
+    repositorio.createActivity('a','b','c')
+    expect(repositorio.getAllActivities()).not.toBe(undefined)
   })
   it('Debe tener el metodo getAllActivities', () => {
-    const actividades = new Activity()
-    expect(actividades.getAllActivities()).toBeDefined()
+    expect(repositorio.getAllActivities()).toBeDefined()
   })
   it('El metodo getAllActivities debe ser un Array', () => {
-    const actividades = new Activity()
-    expect(Array.isArray(actividades.getAllActivities())).toBeTrue()
+    expect(Array.isArray(repositorio.getAllActivities())).toBeTrue()
   })
   it('Debe tener el metodo deleteActivity', () => {
-    const actividades = new Activity()
-    expect(actividades.deleteActivity).toBeDefined()
+    expect(repositorio.deleteActivity).toBeDefined()
   })
   
 })
